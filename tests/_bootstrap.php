90ee8bc9c46baa628160c76c899f25a1759d246b
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-08-13 01:47:48 +0200

    Updates tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,14 +1,22 @@
 <?php
 // This is global bootstrap for autoloading
 
-$_SERVER['doc_root']     = __DIR__ . '/../../../../';
-$_SERVER['app_path']     = __DIR__ . '/../../../app/';
-$_SERVER['core_path']    = __DIR__ . '/../../../core/';
-$_SERVER['package_path'] = __DIR__ . '/../../';
-$_SERVER['vendor_path']  = __DIR__ . '/../../../vendor/';
+$fuel = realpath(__DIR__ . '/../../../..');
+$package = null;
 
-require_once __DIR__ . '/../../../core/bootstrap_phpunit.php';
-require_once __DIR__ . '/../vendor/autoload.php';
+if ($travis = getenv('TRAVIS'))
+{
+	$fuel = '/tmp/fuel';
+	$package = getenv('TRAVIS_BUILD_DIR').'/';
+}
 
-\Package::load('sms');
+$_SERVER['doc_root']     = $fuel;
+$_SERVER['app_path']     = $fuel . '/fuel/app';
+$_SERVER['core_path']    = $fuel . '/fuel/core';
+$_SERVER['package_path'] = $fuel . '/fuel/packages';
+$_SERVER['vendor_path']  = $fuel . '/fuel/vendor';
+
+require_once $_SERVER['core_path'] . '/bootstrap_phpunit.php';
+
+\Package::load('sms', $package);
 \Composer::package('sms');
