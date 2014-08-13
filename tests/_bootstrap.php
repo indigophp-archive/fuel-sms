ab7d64e13bfd5405dee216961b08d01cce136518
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-08-13 13:02:52 +0200

    Adds Codeception Fuel module

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,22 +1,4 @@
 <?php
 // This is global bootstrap for autoloading
 
-$fuel = realpath(__DIR__ . '/../../../..');
-$package = null;
-
-if ($travis = getenv('TRAVIS'))
-{
-	$fuel = '/tmp/fuel';
-	$package = getenv('TRAVIS_BUILD_DIR').'/';
-}
-
-$_SERVER['doc_root']     = $fuel;
-$_SERVER['app_path']     = $fuel . '/fuel/app';
-$_SERVER['core_path']    = $fuel . '/fuel/core';
-$_SERVER['package_path'] = $fuel . '/fuel/packages';
-$_SERVER['vendor_path']  = $fuel . '/fuel/vendor';
-
-require_once $_SERVER['core_path'] . '/bootstrap_phpunit.php';
-
-\Package::load('sms', $package);
-\Composer::package('sms');
+require __DIR__.'/../vendor/autoload.php';
